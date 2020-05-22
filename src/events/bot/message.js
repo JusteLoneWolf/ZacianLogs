b59c60392acd04ee11b9b0688c9ed57d2796904b
zechaos031 <delrieu.adrien@gmail.com>
2020-05-22 22:20:02 +0200

    [Adding] anti insulte add

--- src/events/bot/message.js
+++ src/events/bot/message.js
@@ -1,13 +1,22 @@
+const AntiInsulte = require('../../modules/antiInsulte')
+
 module.exports = class {
     constructor(client) {
         this.client = client;
     }
 
     run(message) {
-        if (message.author.bot || !message.content.startsWith(this.client.config.prefix)) return;
+
+        this.client.guildDB.ensure(message.guild.id, {
+            prefix: this.client.config.prefix,
+        });
+
+        const insulte = new AntiInsulte(this.client);
+        insulte.run(message);
+        if (message.author.bot || !message.content.startsWith(this.client.guildDB.get(message.guild.id, "prefix"))) return;
 
         const args = message.content.split(/\s+/g);
-        const command = args.shift().slice(this.client.config.prefix.length);
+        const command = args.shift().slice(this.client.guildDB.get(message.guild.id, "prefix").length);
         const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
 
         if (!cmd) return;
---
+++ src/modules/antiInsulte.js
@@ -0,0 +1,11 @@
+class AntiInsulte {
+    constructor(client){
+        this.client = client
+    }
+
+    run(message){
+
+    }
+}
+
+module.exports = AntiInsulte;
