23862910ccf6d9fcb91a981d97d2894744da4eab
zechaos031 <delrieu.adrien@gmail.com>
2020-05-23 18:05:46 +0200

    [Update] AntiInsulte System

---
+++ src/events/logs/InsulteLogs.js
@@ -0,0 +1,37 @@
+module.exports = class {
+    constructor(client) {
+        this.client = client;
+    }
+    run(message,data){
+
+        let channel = message.guild.channels.cache.get(this.client.guildDB.get(message.guild.id,'channels.logs'))
+
+        if(!channel )return
+
+        message.channel.send({
+            embed:{
+                title:'Anti Insulte Logs',
+                description:'Message supprimer',
+                color :0xF5AD2E,
+                fields:[
+                    {
+                        name:'❱ Utilisateur',
+                        value : message.author.username
+                    },
+                    {
+                        name:'❱ Contenu',
+                        value:message.content.length > 1090 ? message.content.substr(0,10) +'...' : message.content
+                    },
+                    {
+                        name:'❱ Roles',
+                        value:message.member.roles.cache.size<0 ? message.member.roles.cache.filter(filter => filter.name !== '@everyone').map(role => `${role.name}`).join(' - ') : 'Pas de roles'
+                    },
+                    {
+                        name:'❱ Mauvais mot',
+                        value: data,
+                    }
+                ]
+            }
+        })
+    }
+};
--- src/modules/antiInsulte.js
+++ src/modules/antiInsulte.js
@@ -2,9 +2,46 @@ class AntiInsulte {
     constructor(client){
         this.client = client
     }
+    run(message) {
+        if(message.author.bot) return;
+        const db = this.client.guildDB.get(message.guild.id);
+        if (!db.badwords.active) return;
+        if (db.badwords.ignore_members.includes(message.author.id) || db.badwords.ignore_channel.includes(message.channel.id)) return;
+        let roles = this.getRoles(message);
+        for(let role =0;role<roles.length;role++){
+            db.badwords.ignore_role.forEach(id =>{
+                if(roles.includes(id)) return
+            });
+            return;
+        }
+        if (db.badwords.custom_words) {
+            let msg = message.content.split(' ')
 
-    run(message){
+            for(let word = 0; word < db.badwords.list.length;word++ ){
+                if (db.badwords.list.includes(msg[word])) {
+                    message.delete().then(() => {
+                        let data = msg[word]
+                        return  message.channel.send('This message is in badwords').then(()=>{
+                            this.client.emit('InsulteLogs',message,data)
+                        })
+                    });
+                    return;
+                }
 
+            }
+
+        }
+
+
+
+    }
+
+    getRoles(message){
+        let roles = [];
+          message.member.roles.cache.filter(filter => filter.name !== '@everyone').forEach(data =>{
+               roles.push(data.id)
+          });
+        return roles
     }
 }
 
