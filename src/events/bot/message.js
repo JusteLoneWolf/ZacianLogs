8edeae9aaf6700d6f806ba7114cb27572a2004af
zechaos031 <delrieu.adrien@gmail.com>
2020-05-28 11:44:54 +0200

    [UPDATE] fix eval

--- src/events/bot/message.js
+++ src/events/bot/message.js
@@ -40,8 +40,9 @@ module.exports = class {
         insulte.run(message);
         if (message.author.bot || !message.content.startsWith(this.client.guildDB.get(message.guild.id, "prefix"))) return;
 
-        const args = message.content.split(/\s+/g);
-        const command = args.shift().slice(this.client.guildDB.get(message.guild.id, "prefix").length);
+        const args = message.content.split(' ').slice(1);
+
+        const command = message.content.split(' ')[0].slice(this.client.guildDB.get(message.guild.id, "prefix").length);
         const cmd = this.client.commands.get(command) || this.client.commands.get(this.client.aliases.get(command));
 
         if (!cmd) return;
