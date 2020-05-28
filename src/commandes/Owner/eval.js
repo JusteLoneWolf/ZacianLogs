47770e2e357246a049a566edff0878e67e68daec
zechaos031 <delrieu.adrien@gmail.com>
2020-05-28 02:16:33 +0200

    [BROKEN] eval broken for annoying reason

--- src/commandes/Owner/eval.js
+++ src/commandes/Owner/eval.js
@@ -9,40 +9,21 @@ class Eval extends Command{
     async run(message,args){
         const initialTime = process.hrtime();
         try {
-            let code = args.join(" ");
-            console.log(code)
+            let code = args.join(' ');
             let evaled = eval(code);
-            if (typeof evaled !== "string")
-                evaled = require("util").inspect(evaled);
+            if (typeof evaled !== 'string')
+                evaled = require('util').inspect(evaled);
             if(evaled.includes(this.client.config.token)){
-                evaled = evaled.replace(this.client.config.token,"TOKEN")
+                evaled = evaled.replace(this.client.config.token,'TOKEN')
             }
             if(evaled .length > 2000) {
                 evaled  = evaled.substr(0, 1980);
                 evaled = evaled + "\nTrop long..";
             }
 
-            if(code.length > 2000) {
-                code  = code.substr(0, 1980);
-                code = code + "\nTrop long..";
-            }
-
             const evalDiff = process.hrtime(initialTime);
-            await message.channel.send({
-                embed:{
-                    fields:[{
-                        name:"Code",
-                        value:`\`\`\`js\n ${code} \`\`\``
-                    },
-                        {
-                            name:"Resultat",
-                            value:`\`\`\`js\n ${evaled} \`\`\``
-                        }],
-                    footer:{
-                        text:evalDiff[0] > 0 ? `${evalDiff[0]}s ${evalDiff[1] / 1000000}ms` : `${evalDiff[1] / 100000}ms`
-                    }
-                }
-            })
+            await message.channel.send('```js\n' + evaled + '```');
+            message.channel.send(evalDiff[0] > 0 ? `${evalDiff[0]}s ${evalDiff[1] / 1000000}ms` : `${evalDiff[1] / 100000}ms`)
 
         } catch (err) {
             console.log(err)
@@ -51,3 +32,4 @@ class Eval extends Command{
 }
 
 module.exports = Eval;
+
