ef836f26a8d1fe687ee3f69821bfacc827e5a66d
zechaos031 <delrieu.adrien@gmail.com>
2020-06-03 13:25:24 +0200

    [FIX] negative ms

--- src/commandes/Generics/ping.js
+++ src/commandes/Generics/ping.js
@@ -6,7 +6,7 @@ class Ping extends Command{
     }
 
     run(message){
-        super.respond(`Ping: ${message.createdAt -Date.now()}ms\nPing du bot ${this.client.ws.ping}ms `)
+        super.respond(`Ping: ${Date.now()-message.createdAt}ms\nPing du bot ${this.client.ws.ping}ms `)
     }
 }
 
