62fe2f200cde076d815049eaa2fc10a61efd6617
zechaos031 <delrieu.adrien@gmail.com>
2020-06-03 14:46:31 +0200

    [FIX] syntaxe fix

--- src/events/logs/channelDelete.js
+++ src/events/logs/channelDelete.js
@@ -5,7 +5,7 @@ module.exports = class {
     }
 
     async run(channel) {
-        if(channel.type ==="dm") return
+        if(channel.type ==="dm") return;
 
         let channeldb = channel.guild.channels.cache.get(this.client.guildDB.get(channel.guild.id,"channels.logs"));
         if(!channeldb )return;
