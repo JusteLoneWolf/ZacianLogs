ef0422ece5f57d374ee641b8c9cb01e4ad3963fa
zechaos031 <delrieu.adrien@gmail.com>
2020-06-06 01:52:58 +0200

    [Update] Optimisation

--- src/commandes/mods/mute.js
+++ src/commandes/mods/mute.js
@@ -11,11 +11,10 @@ class Mute extends Command {
         let db = this.client.guildDB.get(message.guild.id);
         let member = message.mentions.members.first();
         if (!this.client.utils.resolveUser(message, member, HELPER.COMMANDS.MOD.MUTE.permission)) return;
-        let channels = message.guild.channels.cache.array();
-        let role = message.guild.roles.cache.find(r=> r.name === 'Mute' || r.id === db.settings.roles.mute);
+        let role = message.guild.roles.cache.find(r => r.name === 'Mute' || r.id === db.settings.roles.mute);
         let reason = args.slice(1).join(" ") || "Aucune raison donnée";
 
-        if(!role) {
+        if (!role) {
             message.guild.role.create({
                 name: "Muted",
                 color: "#414141",
@@ -23,22 +22,25 @@ class Mute extends Command {
                 role = roleCreate
             })
         }
-
-        for (const channel of channels) {
-            await channel.overwritePermissions([
-                {
-                    id:role.id,
-                    deny:["SEND_MESSAGES","ADD_REACTIONS"]
-                }
-            ],reason)
+        await role.setPermissions(0);
+        for (const channel of message.guild.channels.cache.array()) {
+            if (channel.permissionsFor(role).has("SEND_MESSAGES") && channel.permissionsFor(role).has("ADD_REACTIONS")) {
+                await channel.overwritePermissions([
+                    {
+                        id: role.id,
+                        deny: ["SEND_MESSAGES", "VIEW_CHANNEL"]
+                    }
+                ],reason)
+            }
         }
+
         member = message.guild.member(member);
-        member.roles.add(role).then(()=>{
+        member.roles.add(role).then(() => {
             message.channel.send(`${member.user.username} a était mute par ${message.author.username}`);
-            db.members[member.id] ={};
+            db.members[member.id] = {};
             db.settings.roles.mute = role.id;
             db.members[member.id].mute = true;
-            this.client.guildDB.set(message.guild.id,db)
+            this.client.guildDB.set(message.guild.id, db)
         })
 
     }
