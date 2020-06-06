16cddea9fef20351d823f99dc409fa2d7405df6e
zechaos031 <delrieu.adrien@gmail.com>
2020-06-07 00:28:28 +0200

    [UPDATE] update errorHandler

--- src/Utils/errorHandler.js
+++ src/Utils/errorHandler.js
@@ -5,7 +5,6 @@ module.exports = async (client) => {
             if(err.stack) {
                 if (err.stack.includes('An invalid token was provided.')) {
                     client.logger.error('Bad token see config.js for set the token');
-                    require('../utils/MailSender')('DOWN','Bot has no token','component+6f8b4ba4-8ea9-49ae-9523-788f24f11528+major_outage@notifications.statuspage.io.')
                 } else if (err.stack.includes('Missing Permissions')) {
                     client.logger.error('Permission Error')
                 } else if(err.stack.includes(' getaddrinfo ENOTFOUND discordapp.com')) {
