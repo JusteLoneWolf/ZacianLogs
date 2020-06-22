63a2b6bf61b2b28044bd99528c58b0f4fe92dfe3
zechaos031 <delrieu.adrien@gmail.com>
2020-06-22 19:54:40 +0200

    [Update] Some update

--- src/events/logs/messageCitation.js
+++ src/events/logs/messageCitation.js
@@ -13,25 +13,32 @@ module.exports = class {
         let scrapedURL = url[0].split("/");
 
         if(isNaN(scrapedURL[5]) || isNaN(scrapedURL[6])) return
-
-        this.client.guilds.cache.get(message.guild.id).channels.cache.get(scrapedURL[5]).messages.fetch(scrapedURL[6]).then((msg)=>{
-            if(msg) return;
-            let data = [];
-            if(msg.embeds[0] && msg.embeds[0].fields){
-                for(const fields of msg.embeds[0].fields){
-                    data.push(fields)
+        try{
+            this.client.guilds.cache.get(message.guild.id).channels.cache.get(scrapedURL[5]).messages.fetch(scrapedURL[6]).then((msg)=>{
+                if(msg) return;
+                let data = [];
+                if(msg.embeds[0] && msg.embeds[0].fields){
+                    for(const fields of msg.embeds[0].fields){
+                        data.push(fields)
+                    }
                 }
-            }
 
-            return message.channel.send({
-                embed:{
-                    title : `Message de ${msg.author.username}`,
-                    description: `[Le message](${url[0]})\n${msg.embeds[0] ? msg.embeds[0].description.length !== 0 ? msg.embeds[0].description  : 'Pas de description' : msg.content}`,
-                    fields : data,
-                },
+                return message.channel.send({
+                    embed:{
+                        title : `Message de ${msg.author.username}`,
+                        description: `[Le message](${url[0]})\n${msg.embeds[0] ? msg.embeds[0].description.length !== 0 ? msg.embeds[0].description  : 'Pas de description' : msg.content}`,
+                        fields : data,
+                    },
 
+                })
+            }).catch(()=>{
+                return
             })
-        })
+        }catch{
+            return
+        }
+
+
 
 
     }
