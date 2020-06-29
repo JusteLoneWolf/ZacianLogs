8c9f3d078c172b4e28bd99bef681579ae1db51b3
zechaos031 <delrieu.adrien@gmail.com>
2020-06-29 13:05:13 +0200

    [UPDATE] update antiraid message event & Database

--- src/events/antiraid/message.js
+++ src/events/antiraid/message.js
@@ -3,9 +3,28 @@ module.exports = class {
         this.client = client;
 
     }
-    async run(oldMessage,newMessage) {
-
+    async run(message) {
+        if (message.channel.type === "dm") return
+        let guildData = await this.getDataOrCreate(message.guild);
+        if(!guildData.settings.antiraid.enabled) return
     }
 
+    async getDataOrCreate(guild){
+        return new Promise(async (resolve)=>{
+            const {Guild} = require('../../models/index');
+            let data = await this.client.dbmanager.getGuild(guild);
+            if(data){
+                resolve(data)
+            }else{
+                const newGuild= {
+                    GuildId : guild.id
+                };
+                const merged = Object.assign({ _id: mongoose.Types.ObjectId()}, newGuild);
+                data = await new Guild(merged);
+                data.save();
+                resolve(data)
+            }
+        })
 
+    }
 }
