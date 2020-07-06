00fc8c6a9ebaf653413fd4be0d3431372aafbf03
zechaos031 <delrieu.adrien@gmail.com>
2020-07-06 12:49:54 +0200

    [UPDATE] update antiraid module

--- src/modules/antiraid.js
+++ src/modules/antiraid.js
@@ -3,29 +3,71 @@ module.exports ={
 
     },
 
-    lockChannel(){
-
-    },
-
-    getMessage(client,message,data){
-
-        if(!data.advert[message.author.id]){
-
+    lockChannel(guild){
+        for(const channel of guild.channels.cache.array()){
+            //TODO
+            // server lockdown
         }
 
     },
 
-    banAction(){
+   async getMessage(client,message,data){
+
+        if(!data.advert[message.author.id]){
+            data.advert[message.author.id]= {
+                content: message.content,
+                warn:0
+            }
+            await client.dbmanager.updateGuild(message.guild,{advert: data.advert})
+            return
+        }
+
+        if(data.advert[message.author.id].content === message.content){
+            data.advert[message.author.id].warn++
+            await client.dbmanager.updateGuild(message.guild,{advert: data.advert})
+
+        }
+
+        if(data.advert[message.author.id].warn < 3) {
+            switch (data.settings.antiraid.blockServer.sanction) {
+                case "ban":
+                    this.banAction(message.member,message.content)
+                    break
+                case "kick":
+                    this.kickAction(message.member,message.content)
+                    break
+                case "mute":
+                    this.muteAction(message.member,message.content)
+
+                    break
+            }
+        }
 
     },
 
-    kickAction(){
+    banAction(user,content){
+        let reason = 'Spam/raid avec le message "'+content+'"'
+        user.ban(reason)
+    },
 
+    kickAction(user,content){
+        let reason = 'Spam/raid avec le message "'+content+'"'
+        user.kick(reason)
     },
 
     muteAction(){
 
     },
+
+    removeWarns(){
+
+    },
+
+
+    beginAntiraid(){
+
+    },
+
 
     endAntiraid(){
 
