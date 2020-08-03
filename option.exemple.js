d9a644edd44e055c4c3fdd34182f8f36a5d2d478
zechaos031 <delrieu.adrien@gmail.com>
2020-08-04 00:54:15 +0200

    [ADD] Adding exemple

---
+++ option.exemple.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+module.exports = {
+    config:{
+        prefix : "zac!",
+        token: process.env.TOKEN,
+        owner: []
+    },
+    clientOption:{
+        ws:{intents:[
+                "GUILDS",
+                "GUILD_BANS",
+                "GUILD_INVITES",
+                "GUILD_MESSAGES",
+                "DIRECT_MESSAGES"
+            ]},
+        disableEveryone: true,
+        fetchAllMembers: false,
+        messageCacheMaxSize: 100,
+        messageSweepInterval: 120,
+        partials:["GUILD_MEMBER","MESSAGE","USER","CHANNEL"]
+    },
+    perm:{},
+    DBconnection: process.env.Dbconnect
+};
