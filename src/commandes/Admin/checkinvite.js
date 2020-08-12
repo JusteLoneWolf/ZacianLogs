b5d17af51527fbb532644f892ac639c28ac23f14
zechaos031 <delrieu.adrien@gmail.com>
2020-08-13 01:55:36 +0200

    [Adding] add checkinvite command

---
+++ src/commandes/Admin/checkinvite.js
@@ -0,0 +1,42 @@
+const Command = require("../../Base/Command");
+const {HELPER } = require("../../Utils/Constant/CommandeHelper");
+class Checkinvite extends Command{
+    constructor(client){
+        super(client,HELPER.COMMANDS.MOD.CHECKINVITE);
+    }
+
+    run(message,args){
+        if(!args[0]) return message.channel.send('Merci de spécifier une invitation')
+        this.client.fetchInvite(args[0]).then(invite => {
+            console.log(invite)
+            message.channel.send({
+                embed:{
+                    fields:[
+                        {
+                            name:"ID du serveur",
+                            value:invite.guild.id
+                        },
+                        {
+                            name:"Nom du serveur de l'invitation",
+                            value:invite.guild.name
+                        },
+                        {
+                            name:"Nom du channel de l'invitation",
+                            value: invite.channel
+                        },
+                        {
+                            name:"Nom du createur de l'invitation",
+                            value: invite.inviter.username
+                        },
+                        {
+                            name:"Nombre de membre",
+                            value: invite.guild.memberCount ? invite.guild.memberCount : 'Impossible de recupérer le nombre de membre'
+                        }
+                    ]
+                }
+            })
+        })
+    }
+}
+
+module.exports = Checkinvite;
