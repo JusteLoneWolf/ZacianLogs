7d17c569e46e357f505655c3257c707c19d32fb1
zechaos031 <delrieu.adrien@gmail.com>
2020-11-23 08:43:51 +0100

    :wrench: some fix

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -36,28 +36,32 @@ class Utils {
             return false
         }
 
-        if(!member) {
-            message.channel.send("Tu doit mentionné un utilisateur");
-            return false
-        }
-        if(member.id === message.author.id){
-            message.channel.send("Tu ne peux pas faire ça sur toi même");
-            return false
-        }
-
-        if(member.id === message.guild.ownerID){
-            message.channel.send("Tu ne peux pas faire ca sur l'owner");
-            return false
-        }
-
-        let user = message.guild.member(member);
-
-        if(message.author.id !== message.guild.ownerID){
-            if(user.role.highest >= message.guild.member(message.member).roles.highest){
-                message.channel.send("Le member mentionné a un role plus haut que toi");
+        if(member !== null){
+            if(!member) {
+                message.channel.send("Tu doit mentionné un utilisateur");
                 return false
             }
+            if(member.id === message.author.id){
+                message.channel.send("Tu ne peux pas faire ça sur toi même");
+                return false
+            }
+
+            if(member.id === message.guild.ownerID){
+                message.channel.send("Tu ne peux pas faire ca sur l'owner");
+                return false
+            }
+
+            let user = message.guild.member(member);
+
+            if(message.author.id !== message.guild.ownerID){
+                if(user.role.highest >= message.guild.member(message.member).roles.highest){
+                    message.channel.send("Le member mentionné a un role plus haut que toi");
+                    return false
+                }
+            }
         }
+
+
         return true
     }
 }
