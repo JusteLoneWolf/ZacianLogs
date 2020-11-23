9dc00df662c8c1e1896a40c019e083a26090a090
zechaos031 <delrieu.adrien@gmail.com>
2020-11-23 08:43:26 +0100

    :package: ADD unlockdown command

---
+++ src/commandes/mods/unlockdown.js
@@ -0,0 +1,21 @@
+const Command = require("../../Base/Command");
+const {HELPER } = require("../../Utils/Constant/CommandeHelper");
+const {unlockChannel} = require('../../modules/antiraid')
+class unLockdown extends Command{
+    constructor(client){
+        super(client,HELPER.COMMANDS.MOD.UNLOCKDOWN);
+        this.client = client
+
+    }
+
+    async run(message,args){
+        if (!this.client.utils.resolveUser(message,null, HELPER.COMMANDS.MOD.MUTE.permission)) return;
+        unlockChannel(message.guild).then((isFinish)=>{
+            if(isFinish){
+                message.channel.send("Le serveur a etait débloqué")
+            }
+        })
+    }
+}
+
+module.exports = unLockdown;
